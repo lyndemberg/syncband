bbd5f0488ca42b56a1cffc051a58439cfd9a960a
lyndemberg <lyndembergbatista@outlook.com>
2019-03-19 17:16:37 -0300

    iniciando busca server

--- app/src/main/java/com/mesh/syncband/fragments/HomeFragment.java
+++ app/src/main/java/com/mesh/syncband/fragments/HomeFragment.java
@@ -8,22 +8,39 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.mesh.syncband.R;
 
 public class HomeFragment extends Fragment {
 
+    private TextView status;
+    private TextView currentBpm;
+    private TextView currentSong;
+    private Button buttonDisconnect;
+    private LinearLayout layoutVolume;
+    private Button buttonSearch;
 
     public HomeFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View inflate = inflater.inflate(R.layout.fragment_home, container, false);
-        return inflate;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        buttonSearch = view.findViewById(R.id.button_search);
+
+        status = view.findViewById(R.id.status);
+        currentBpm = view.findViewById(R.id.current_bpm);
+        currentSong = view.findViewById(R.id.current_song);
+        buttonDisconnect = view.findViewById(R.id.button_disconnect);
+        layoutVolume = view.findViewById(R.id.layout_volume);
+
+        return view;
     }
 
 
@@ -33,4 +50,8 @@ public class HomeFragment extends Fragment {
         getActivity().setTitle("SyncBand - Home");
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 }
